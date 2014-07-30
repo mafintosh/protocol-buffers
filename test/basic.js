85f2c4cd29893e6cb36a475b910f5001cc8d3547
Mathias Buus <mathiasbuus@gmail.com>
2014-07-30 15:28:33 +0200

    fix tests

--- test/basic.js
+++ test/basic.js
@@ -1,62 +1,56 @@
-var tape = require('tape');
-var path = require('path');
-var protobuf = require('../');
-var spec = require('./basic.json');
+var tape = require('tape')
+var path = require('path')
+var protobuf = require('../require')
+var Basic = protobuf('./test.proto').Basic
 
-tape('encode', function(t) {
-	var schema = protobuf(spec);
-	var b1 = schema.encode({
-		num: 1,
-		payload: new Buffer('lol')
-	});
+tape('basic encode', function(t) {
+  var b1 = Basic.encode({
+    num: 1,
+    payload: new Buffer('lol')
+  })
 
-	t.ok(b1);
+  var b2 = Basic.encode({
+    num: 1,
+    payload: new Buffer('lol'),
+    meeeh: 42
+  })
 
-	var b2 = schema.encode({
-		num: 1,
-		payload: new Buffer('lol'),
-		meeeh: 42
-	});
+  t.same(b2, b1)
+  t.end()
+})
 
-	t.same(b2.length, b1.length);
-	t.end();
-});
+tape('basic encode + decode', function(t) {
+  var b1 = Basic.encode({
+    num: 1,
+    payload: new Buffer('lol')
+  })
 
-tape('encode + decode', function(t) {
-	var schema = protobuf(spec);
-	var b1 = schema.encode({
-		num: 1,
-		payload: new Buffer('lol')
-	});
+  var o1 = Basic.decode(b1)
 
-	var o1 = schema.decode(b1);
+  t.same(o1.num, 1)
+  t.same(o1.payload, new Buffer('lol'))
 
-	t.same(o1.num, 1);
-	t.same(o1.payload, new Buffer('lol'));
+  var b2 = Basic.encode({
+    num: 1,
+    payload: new Buffer('lol'),
+    meeeh: 42
+  })
 
+  var o2 = Basic.decode(b1)
 
-	var b2 = schema.encode({
-		num: 1,
-		payload: new Buffer('lol'),
-		meeeh: 42
-	});
+  t.same(o2, o1)
+  t.end()
+})
 
-	var o2 = schema.decode(b1);
+tape('basic encode + decode floats', function(t) {
+  var b1 = Basic.encode({
+    num: 1.1,
+    payload: new Buffer('lol')
+  })
 
-	t.same(o2, o1);
-	t.end();
-});
+  var o1 = Basic.decode(b1)
 
-tape('encode + decode floats', function(t) {
-	var schema = protobuf(spec);
-	var b1 = schema.encode({
-		num: 1.1,
-		payload: new Buffer('lol')
-	});
-
-	var o1 = schema.decode(b1);
-
-	t.same(o1.num, 1.1);
-	t.same(o1.payload, new Buffer('lol'));
-	t.end();
-});
+  t.same(o1.num, 1.1)
+  t.same(o1.payload, new Buffer('lol'))
+  t.end()
+})
--- test/nested.js
+++ test/nested.js
@@ -1,66 +1,62 @@
-var tape = require('tape');
-var path = require('path');
-var protobuf = require('../');
-var spec = require('./nested.json');
+var tape = require('tape')
+var path = require('path')
+var protobuf = require('../require')
+var Nested = protobuf('./test.proto').Nested
 
-tape('encode', function(t) {
-	var schema = protobuf(spec);
-	var b1 = schema.encode({
-		num: 1,
-		payload: new Buffer('lol'),
-		meh: {
-			num: 2,
-			payload: new Buffer('bar')
-		}
-	});
+tape('nested encode', function(t) {
+  var b1 = Nested.encode({
+    num: 1,
+    payload: new Buffer('lol'),
+    meh: {
+      num: 2,
+      payload: new Buffer('bar')
+    }
+  })
 
-	t.ok(b1);
+  var b2 = Nested.encode({
+    num: 1,
+    payload: new Buffer('lol'),
+    meeeh: 42,
+    meh: {
+      num: 2,
+      payload: new Buffer('bar')
+    }
+  })
 
-	var b2 = schema.encode({
-		num: 1,
-		payload: new Buffer('lol'),
-		meeeh: 42,
-		meh: {
-			num: 2,
-			payload: new Buffer('bar')
-		}
-	});
+  t.same(b2, b1)
+  t.end()
+})
 
-	t.same(b2.length, b1.length);
-	t.end();
-});
+tape('nested encode + decode', function(t) {
+  var b1 = Nested.encode({
+    num: 1,
+    payload: new Buffer('lol'),
+    meh: {
+      num: 2,
+      payload: new Buffer('bar')
+    }
+  })
 
-tape('encode + decode', function(t) {
-	var schema = protobuf(spec);
-	var b1 = schema.encode({
-		num: 1,
-		payload: new Buffer('lol'),
-		meh: {
-			num: 2,
-			payload: new Buffer('bar')
-		}
-	});
+  var o1 = Nested.decode(b1)
 
-	var o1 = schema.decode(b1);
+  t.same(o1.num, 1)
+  t.same(o1.payload, new Buffer('lol'))
+  t.ok(o1.meh, 'has nested property')
+  t.same(o1.meh.num, 2)
+  t.same(o1.meh.payload, new Buffer('bar'))
 
-	t.same(o1.num, 1);
-	t.same(o1.payload, new Buffer('lol'));
-	t.ok(o1.meh);
-	t.same(o1.meh.num, 2);
-	t.same(o1.meh.payload, new Buffer('bar'));
+  var b2 = Nested.encode({
+    num: 1,
+    payload: new Buffer('lol'),
+    meeeh: 42,
+    meh: {
+      num: 2,
+      payload: new Buffer('bar')
+    }
+  });
 
-	var b2 = schema.encode({
-		num: 1,
-		payload: new Buffer('lol'),
-		meeeh: 42,
-		meh: {
-			num: 2,
-			payload: new Buffer('bar')
-		}
-	});
+  var o2 = Nested.decode(b1)
 
-	var o2 = schema.decode(b1);
-
-	t.same(o2, o1);
-	t.end();
-});
+  t.same(o2, o1)
+  t.end()
+})
