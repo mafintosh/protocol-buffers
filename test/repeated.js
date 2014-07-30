723156868a2f26d619be04fdb31b76d1378b1704
Mathias Buus <mathiasbuus@gmail.com>
2014-07-30 15:35:55 +0200

    add test for repeated

---
+++ test/repeated.js
@@ -0,0 +1,68 @@
+var tape = require('tape')
+var path = require('path')
+var protobuf = require('../require')
+var Repeated = protobuf('./test.proto').Repeated
+
+tape('repeated encode', function(t) {
+  var b1 = Repeated.encode({
+    list: [{
+      num: 1,
+      payload: new Buffer('lol')
+    }, {
+      num: 2,
+      payload: new Buffer('lol1')
+    }]
+  })
+
+  var b2 = Repeated.encode({
+    list: [{
+      num: 1,
+      payload: new Buffer('lol')
+    }, {
+      num: 2,
+      payload: new Buffer('lol1'),
+      meeeeh: 100
+    }],
+    meeh: 42
+  })
+
+  t.same(b2, b1)
+  t.end()
+})
+
+tape('repeated encode + decode', function(t) {
+  var b1 = Repeated.encode({
+    list: [{
+      num: 1,
+      payload: new Buffer('lol')
+    }, {
+      num: 2,
+      payload: new Buffer('lol1')
+    }]
+  })
+
+  var o1 = Repeated.decode(b1)
+
+  t.same(o1.list.length, 2)
+  t.same(o1.list[0].num, 1)
+  t.same(o1.list[0].payload, new Buffer('lol'))
+  t.same(o1.list[1].num, 2)
+  t.same(o1.list[1].payload, new Buffer('lol1'))
+
+  var b2 = Repeated.encode({
+    list: [{
+      num: 1,
+      payload: new Buffer('lol')
+    }, {
+      num: 2,
+      payload: new Buffer('lol1'),
+      meeeeh: 100
+    }],
+    meeh: 42
+  })
+
+  var o2 = Repeated.decode(b1)
+
+  t.same(o2, o1)
+  t.end()
+})
