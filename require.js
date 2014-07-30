9d2150bf1bfa3fd634412c5ecb3a0e63623287f8
Mathias Buus <mathiasbuus@gmail.com>
2014-07-30 14:42:16 +0200

    fix require

--- require.js
+++ require.js
@@ -5,7 +5,7 @@ var protobuf = require('./')
 delete require.cache[require.resolve(__filename)]
 
 module.exports = function(file) {
-  var parent = path.dirname(module.parent.filename)
+  file = path.join(path.dirname(module.parent.filename), file)
   if (!fs.existsSync(file) && fs.existsSync(file+'.proto')) file += '.proto'
   return protobuf(fs.readFileSync(file, 'utf-8'))
 }
