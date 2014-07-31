8cc12f80ec0d8f192cdf3a202f15aead6ddb6b21
Mathias Buus <mathiasbuus@gmail.com>
2014-07-31 11:04:17 +0200

    support for encodings in require

--- require.js
+++ require.js
@@ -4,8 +4,8 @@ var protobuf = require('./')
 
 delete require.cache[require.resolve(__filename)]
 
-module.exports = function(file) {
+module.exports = function(file, encodings) {
   file = path.join(path.dirname(module.parent.filename), file)
   if (!fs.existsSync(file) && fs.existsSync(file+'.proto')) file += '.proto'
-  return protobuf(fs.readFileSync(file, 'utf-8'))
+  return protobuf(fs.readFileSync(file, 'utf-8'), encodings)
 }
