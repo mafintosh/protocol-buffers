f51e8f80e53e1529fd1988a9e8a6aee0bccb7c14
Mathias Buus <mathiasbuus@gmail.com>
2014-10-12 13:45:43 +0800

    use resolve-protobuf-schema

--- require.js
+++ require.js
@@ -1,29 +1,10 @@
-var fs = require('fs')
+// TODO: this should be removed in 3.0.0 - just use the two modules instead
+var resolve = require('resolve-protobuf-schema')
 var path = require('path')
-var schema = require('protobuf-schema')
 var protobuf = require('./')
 
 delete require.cache[require.resolve(__filename)]
 
-var importSchema = function(filename) {
-  if (!fs.existsSync(filename) && fs.existsSync(filename+'.proto')) filename += '.proto'
-
-  var dir = path.dirname(filename)
-  var sch = schema(fs.readFileSync(filename, 'utf-8'))
-
-  if (!sch.imports || !sch.imports.length) return sch
-
-  sch.imports.forEach(function(i) {
-    var ch = importSchema(path.join(dir, i))
-    sch.messages = sch.messages.concat(ch.messages)
-    sch.enums = sch.enums.concat(ch.enums)
-  })
-
-  return sch
-}
-
 module.exports = function(filename, opts) {
-  var dir = path.dirname(module.parent.filename)
-  var sch = importSchema(path.join(path.dirname(module.parent.filename), filename))
-  return protobuf(sch, opts)
+  return protobuf(resolve.sync(path.resolve(path.dirname(module.parent.filename), filename)), opts)
 }
