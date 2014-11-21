51d3cd90cfc09fa57a036d5fe538af4d3b0de045
Mathias Buus <mathiasbuus@gmail.com>
2014-11-21 01:17:34 +0100

    add failing test for #15

---
+++ test/utf-8.js
@@ -0,0 +1,15 @@
+var tape = require('tape')
+var path = require('path')
+var protobuf = require('../require')
+var UTF8 = protobuf('./test.proto').UTF8
+
+tape('strings can be utf-8', function(t) {
+  var ex = {
+    foo: 'ビッグデータ「人間の解釈が必要」「量の問題ではない」論と、もう一つのビッグデータ「人間の解釈が必要」「量の問題ではない」論と、もう一つの',
+    bar: 42
+  }
+  var b1 = UTF8.encode(ex)
+
+  t.same(UTF8.decode(b1), ex)
+  t.end()
+})
