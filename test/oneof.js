665599c8df7d6355302b2bee128a74d6dd8c1670
Brendan Ward <bcward@consbio.org>
2015-02-05 06:44:26 -0800

    Added oneof support

---
+++ test/oneof.js
@@ -0,0 +1,58 @@
+var tape = require('tape')
+var path = require('path')
+var protobuf = require('../require')
+var proto = protobuf('./test.proto')
+var Property = proto.Property
+var PropertyNoOneof = proto.PropertyNoOneof
+
+var data = {
+  name: 'Foo',
+  desc: 'optional description',
+  int_value: 12345
+}
+
+tape('oneof encode', function(t) {
+  t.ok(Property.encode(data), 'oneof encode')
+  t.end()
+})
+
+tape('oneof encode + decode', function(t) {
+  var buf = Property.encode(data);
+  var out = Property.decode(buf)
+  t.deepEqual(data, out)
+  t.end()
+})
+
+tape('oneof encode + decode of overloaded oneof json', function(t) {
+  var invalidData = {
+    name: 'Foo',
+    desc: 'optional description',
+    string_value: 'Bar', // ignored
+    bool_value:  true,  // ignored
+    int_value: 12345 // retained, was last entered
+  }
+  var buf = Property.encode(invalidData);
+  var out = Property.decode(buf)
+  t.deepEqual(data, out)
+  t.end()
+})
+
+tape('oneof encode + decode of overloaded oneof buffer', function(t) {
+  var invalidData = {
+    name: 'Foo',
+    desc: 'optional description',
+    string_value: 'Bar', // retained, has highest tag number
+    bool_value:  true,  // ignored
+    int_value: 12345 // ignored
+  }
+  var validData = {
+    name: 'Foo',
+    desc: 'optional description',
+    string_value: 'Bar'
+  }
+
+  var buf = PropertyNoOneof.encode(invalidData);
+  var out = Property.decode(buf)
+  t.deepEqual(validData, out)
+  t.end()
+})
