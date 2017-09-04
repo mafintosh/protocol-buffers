122d5c0ba35a457c2513aa0292097495e6ac1e33
dignifiedquire <dignifiedquire@gmail.com>
2017-09-04 19:14:27 +0200

    fix: updated for packed fixes

--- test/packed.js
+++ test/packed.js
@@ -6,17 +6,17 @@ var Packed = protobuf(fs.readFileSync(__dirname + '/test.proto')).Packed
 tape('Packed encode', function (t) {
   var b1 = Packed.encode({
     packed: [
-      'hello world',
-      'hej verden',
-      'hola mundo'
+      12,
+      13,
+      14
     ]
   })
 
   var b2 = Packed.encode({
     packed: [
-      'hello world',
-      'hej verden',
-      'hola mundo'
+      12,
+      13,
+      14
     ],
     meeh: 42
   })
@@ -28,89 +28,25 @@ tape('Packed encode', function (t) {
 tape('Packed encode + decode', function (t) {
   var b1 = Packed.encode({
     packed: [
-      'hello world',
-      'hej verden',
-      'hola mundo'
+      12,
+      13,
+      14
     ]
   })
 
   var o1 = Packed.decode(b1)
 
   t.same(o1.packed.length, 3)
-  t.same(o1.packed[0], 'hello world')
-  t.same(o1.packed[1], 'hej verden')
-  t.same(o1.packed[2], 'hola mundo')
+  t.same(o1.packed[0], 12)
+  t.same(o1.packed[1], 13)
+  t.same(o1.packed[2], 14)
 
   var b2 = Packed.encode({
     packed: [
-      'hello world',
-      'hej verden',
-      'hola mundo'
+      12,
+      13,
+      14
     ],
-    meeh: 42
-  })
-
-  var o2 = Packed.decode(b2)
-
-  t.same(o2, o1)
-  t.end()
-})
-
-tape('packed message encode', function (t) {
-  var b1 = Packed.encode({
-    list: [{
-      num: 1,
-      payload: new Buffer('lol')
-    }, {
-      num: 2,
-      payload: new Buffer('lol1')
-    }]
-  })
-
-  var b2 = Packed.encode({
-    list: [{
-      num: 1,
-      payload: new Buffer('lol')
-    }, {
-      num: 2,
-      payload: new Buffer('lol1'),
-      meeeeh: 100
-    }],
-    meeh: 42
-  })
-
-  t.same(b2, b1)
-  t.end()
-})
-
-tape('packed message encode + decode', function (t) {
-  var b1 = Packed.encode({
-    list: [{
-      num: 1,
-      payload: new Buffer('lol')
-    }, {
-      num: 2,
-      payload: new Buffer('lol1')
-    }]
-  })
-
-  var o1 = Packed.decode(b1)
-
-  t.same(o1.list.length, 2)
-  t.same(o1.list[0].num, 1)
-  t.same(o1.list[0].payload, new Buffer('lol'))
-  t.same(o1.list[1].num, 2)
-  t.same(o1.list[1].payload, new Buffer('lol1'))
-
-  var b2 = Packed.encode({
-    list: [{
-      num: 1,
-      payload: new Buffer('lol')
-    }, {
-      num: 2,
-      payload: new Buffer('lol1'),
-      meeeeh: 100
-    }],
     meeh: 42
   })
 
